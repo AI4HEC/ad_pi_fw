52975af829150bea8cc185fe7372dd7b1fd7d2cb
ThimiH <e19276@eng.pdn.ac.lk>
2025-06-13 00:09:25 +0530

    Update configuration settings and enhance ConfigManager class with comprehensive methods

---
+++ src/utils/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Filename: __init__.py
+Author: Thimira Hirushan
+Date: 2025-06-13
+Description: This module initializes the utils package and imports the ConfigManager class for managing configuration settings 
+             and import helpers for various utility functions.
+Version: 1.0
+"""
+
+from .config import ConfigManager
